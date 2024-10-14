7b6870ac3f65174e8d19280c427ee81fae7769a1
Jacob <jacobpayne007@gmail.com>
2024-10-14 11:33:24 -0500

    done

--- lib/employee.py
+++ lib/employee.py
@@ -195,7 +195,7 @@ class Employee:
             where employee_id = ?
         """
 
-        CURSOR.execute(sql,(self.id,),)
+        CURSOR.execute(sql,(self.id,))
 
         rows = CURSOR.fetchall()
         return [Review.instance_from_db(row) for row in rows]
